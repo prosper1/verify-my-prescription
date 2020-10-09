827d46baa1ee25dbd533f1afdbd7a28d0619d39d
gundotshili@gmail.com <gundotshili@gmail.com>
2020-10-09 21:10:30 +0200

    added src structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LandingComponent } from './main/landing/landing.component';
+import { PrescriptionComponent } from './main/prescription/prescription.component';
+import { TrackComponent } from './main/track/track.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    LandingComponent,
+    PrescriptionComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule,
